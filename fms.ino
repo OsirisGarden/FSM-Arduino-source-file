83ca0a267fd03f50b4e9421a5248aab3f5b67273
Cristian Pérez Corral <69525230+cperezln@users.noreply.github.com>
2022-04-08 10:35:10 +0200

    Implemented yet the temperature/humidity lector, with relay

--- fms.ino
+++ fms.ino
@@ -1,6 +1,12 @@
+#include <DHT.h>
+#define DHTPIN 2
+
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 // constants won't change. Used here to set a pin number:
-const int ledPin =  13;// the number of the LED pin
+const int ledPinRelay1 =  13;// the number of the LED pin
+const int ledPinRelay2 = 12;
 
 // Variables will change:
 int ledState = HIGH; 
@@ -23,16 +29,21 @@ void loop() {
     float val = analogRead(A0);
     Serial.println(val);
     if(val < 500) {
-      digitalWrite(ledPin, HIGH);
+      digitalWrite(ledPinRelay1, HIGH);
     }
     else {
-      digitalWrite(ledPin, LOW);
+      digitalWrite(ledPinRelay1, LOW);
     }
-    Serial.flush();
-
-
-
-
-
-    
+    delay(5000);
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
+    Serial.println(t);
+    Serial.println(h);
+    if(t > 19) {
+      digitalWrite(ledPinRelay2, LOW);
+    }
+    else {
+      digitalWrite(ledPinRelay2, HIGH);
+    }
+    Serial.flush();    
 }
