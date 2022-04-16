cf4cb267c432728fbb16bfe116950f5b094ef700
Cristian Pérez Corral <69525230+cperezln@users.noreply.github.com>
2022-04-16 14:12:07 +0200

    comunicación "hecha"

--- fms/fms.ino
+++ fms/fms.ino
@@ -1,72 +1,74 @@
-//#include <DHT.h>
+#include <DHT.h>
 #define DHTPIN 2
 
 #define DHTTYPE DHT11
-//DHT dht(DHTPIN, DHTTYPE);
-//CONSTANTES PINES
-const int ledPinRelay1 =  13;// the number of the LED pin
-const int ledPinRelay2 = 12;
-const int ledPinRelay3 = 8;//Acciona rele de la bomba
+DHT dht(DHTPIN, DHTTYPE);
+CONSTANTES PINES
+const int releLuz =  13;// Número del pin
+const int releCalor = 12;
+const int releAgua = 8;//Acciona rele de la bomba
 
 //CONSTANTES HUMEDAD
 const int humedadMax = 180; //Mientras más humedad menor es la señal analógica
 const int humedadMin = 750;
 
 //CONSTANTES LUZ
+const int luzMin = 500;
 
 //CONSTANTES TEMPERATURA
-
+const int minCalor = 19;
 
 void setup() {
-  // set the digital pin as output:
   Serial.begin(9600);
-  //pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-
-    
     //////////////LECTURA VALORES RASPI/////////////
     //Aqui se lee y procesa la informacion pasada por el puerto serial desde raspi a arduino
     //Probablemente haya que cambiar las const a variables
     
-    //////////////GESTION TEMPERATURA///////////////
+    //////////////GESTION HUMEDAD TIERRA///////////////
     float hum_tierra = analogRead(A1);
     if (hum_tierra > humedadMin){
       //Encendemos el rele que acciona la bomba
-      digitalWrite(ledPinRelay3, HIGH);
-    }else{
+      digitalWrite(releAgua, HIGH);
+    }
+    else{
       //Apagamos el rele, hay humedad suficiente
-      digitalWrite(ledPinRelay3, LOW);
+      digitalWrite(releAgua, LOW);
     }
     
     //////////////GESTION LUZ///////////////////////
-
-    
-    //////////////GESTION TEMPERATURA///////////////
-
-    
-    //////////////ESCRITURA VALORES PARA RASPI//////
-    float val = analogRead(A0);
-    //Serial.println(val);
-    if(val < 500) {
-      digitalWrite(ledPinRelay1, HIGH);
+    float luz_int = analogRead(A0);
+    if(luz_int < luzMin) {
+      digitalWrite(releLuz, HIGH);
     }
     else {
-      digitalWrite(ledPinRelay1, LOW);
+      digitalWrite(releLuz, LOW);
     }
-    //delay(5000);
-    //float t = dht.readTemperature();
-    //float h = dht.readHumidity();
-    //Serial.println(t);
-    //Serial.println(h);
-    int t=0;
-    if(t > 19) {
-      digitalWrite(ledPinRelay2, LOW);
+    //////////////GESTION TEMPERATURA Y HUMEDAD AIRE///////////////
+    // HAY QUE HACER THREADING YA QUE ESTE SENSOR PILLA CADA 5 SECS
+    float temp = dht.readTemperature();
+    float hum_aire = dht.readHumidity();
+    if(t > minCalor) {
+      digitalWrite(releCalor, LOW);
     }
     else {
-      digitalWrite(ledPinRelay2, HIGH);
+      digitalWrite(releCalor, HIGH);
     }
-    Serial.flush();    
+    //////////////LECTURA VALORES PARA RASPI//////
+    //HACER EN THREADING
+    String cmd = "";
+    if(Serial.available()) {
+      delay(10);
+      while(Serial.available() > 0){
+        cmd += (char)Serial.read();
+      }
+      Serial.flush();
+    }
+    if(cmd != "") {
+      Serial.print("Comando recibido, procesaremos solicitud");
+    }
+    delay(500);
     
 }
