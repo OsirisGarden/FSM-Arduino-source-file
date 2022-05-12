1900adccf60252f17e1e1ee2547235d99b86a3ec
Cristian Pérez Corral <69525230+cperezln@users.noreply.github.com>
2022-05-12 18:54:23 +0200

    Update fms.ino

--- fms/fms.ino
+++ fms/fms.ino
@@ -1,64 +1,126 @@
-#include <DHT.h>
-#define DHTPIN 2
 
+#include <DHT.h>
+#include <LiquidCrystal.h>
+
+#define DHTPIN 13
 #define DHTTYPE DHT11
+
 DHT dht(DHTPIN, DHTTYPE);
-CONSTANTES PINES
-const int releLuz =  13;// Número del pin
-const int releCalor = 12;
-const int releAgua = 8;//Acciona rele de la bomba
+LiquidCrystal lcd(7,8,9,10,11,12);
+
+
+//CONSTANTES Y VARIABLES MULTITASKING
+float dhtContPrevio = 0;
+float lcdContPrevio = 0;
+float dhtContActual = 0;
+float lcdContActual = 0;
+float printContPrevio = 0;
+float printContActual = 0;
+const float dhtMax = 2000;
+const float lcdMax= 750;
+const float printMax = 1000;
+
+//CONSTANTES PINES
+const int releLuz =  5;// Número del pin
+const int releCalor = 3;
+const int releAgua = 4;//Acciona rele de la bomba
+const int releVentilador = 2; // Acciona el relé de los ventiladores
 
 //CONSTANTES HUMEDAD
-const int humedadMax = 180; //Mientras más humedad menor es la señal analógica
-const int humedadMin = 750;
+int humedadMax = 44; //Mientras más humedad menor es la señal analógica
 
 //CONSTANTES LUZ
-const int luzMin = 500;
+int luzMin = 20;
 
 //CONSTANTES TEMPERATURA
-const int minCalor = 19;
+int minCalor = 20;
+int maxCalor = 21;
 
+// VALORES REGISTRADOS
+float temp = 0;
+float hum_tierra = 0;
+float luz_int = 0;
+float hum_per = 0;
+float luz_per = 0;
 void setup() {
+  pinMode(releLuz, OUTPUT);
+  pinMode(releCalor, OUTPUT);
+  pinMode(releAgua, OUTPUT);
+  pinMode(releVentilador, OUTPUT);
+  analogWrite(6, 20);
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  dht.begin();
+  // Print a message to the LCD.
+  lcd.setCursor(0, 0);
+  lcd.print("L:");
+  lcd.setCursor(8, 0);
+  lcd.print("T: ");
+  lcd.setCursor(0, 1);
+  lcd.print("H:");
+  lcd.setCursor(8, 1);
+  lcd.print("R: ");
 }
 
 void loop() {
-    //////////////LECTURA VALORES RASPI/////////////
-    //Aqui se lee y procesa la informacion pasada por el puerto serial desde raspi a arduino
-    //Probablemente haya que cambiar las const a variables
+  // Contador para el sensor cada x tiempo
+  dhtContActual = millis();
+  // Leemos y pintamos en LCD la temp
+  lcd.setCursor(3,0);
+  luz_int = analogRead(A1) + analogRead(A2);
+  luz_per = luz_int*100/2048;
+  lcd.print(luz_per, 1);
+  // Comprobamos el tiempo del sensor de temp, y además pintamos
+  if(dhtContActual - dhtContPrevio > dhtMax) {
+      temp = dht.readTemperature();
+      dhtContPrevio = dhtContActual;
+  }
+  lcd.setCursor(11, 0);
+  lcd.print(temp, 1);
+  hum_tierra = analogRead(A4);
+  lcd.setCursor(3, 1);
+  // Leemos hhumedad y pintamos en LCD
+  hum_per = 100 - hum_tierra*100/1024;
+  lcd.print(hum_per, 1);
+  lcd.setCursor(11, 1);
+  // Comprobamos si hay riego y pintamos
+  if(hum_per < humedadMax) {
+    lcd.print("SI");
+  }
+  else {
+    lcd.print("NO");
+  }
+  // Comprobamos si la luz está por debajo de mínimos, si es así la encendemos
+  if(luz_per < luzMin) {
+    digitalWrite(releLuz, LOW);
+  }
+  else {
+    digitalWrite(releLuz, HIGH);
+  }
+  //TEMPERATURA
+  if(temp < minCalor) {
+    digitalWrite(releVentilador, HIGH);
+    digitalWrite(releCalor, LOW);
+  }
+  else {
+    digitalWrite(releCalor, HIGH);
+  }
+  if(temp > maxCalor) {
+    digitalWrite(releVentilador, LOW);
+    digitalWrite(releCalor, HIGH);
+  }
+  else {
+    digitalWrite(releVentilador, HIGH);
+  }
+  //HUMEDAD
+  if(hum_per < humedadMax) {
+    digitalWrite(releAgua, LOW);
+  }else{
+    digitalWrite(releAgua, HIGH);
     
-    //////////////GESTION HUMEDAD TIERRA///////////////
-    float hum_tierra = analogRead(A1);
-    if (hum_tierra > humedadMin){
-      //Encendemos el rele que acciona la bomba
-      digitalWrite(releAgua, HIGH);
-    }
-    else{
-      //Apagamos el rele, hay humedad suficiente
-      digitalWrite(releAgua, LOW);
-    }
-    
-    //////////////GESTION LUZ///////////////////////
-    float luz_int = analogRead(A0);
-    if(luz_int < luzMin) {
-      digitalWrite(releLuz, HIGH);
-    }
-    else {
-      digitalWrite(releLuz, LOW);
-    }
-    //////////////GESTION TEMPERATURA Y HUMEDAD AIRE///////////////
-    // HAY QUE HACER THREADING YA QUE ESTE SENSOR PILLA CADA 5 SECS
-    float temp = dht.readTemperature();
-    float hum_aire = dht.readHumidity();
-    if(t > minCalor) {
-      digitalWrite(releCalor, LOW);
-    }
-    else {
-      digitalWrite(releCalor, HIGH);
-    }
-    //////////////LECTURA VALORES PARA RASPI//////
-    //HACER EN THREADING
-    String cmd = "";
+  }
+  // LECTURA/ESCRITURA SERIAL
+  String cmd = "";
     if(Serial.available()) {
       delay(10);
       while(Serial.available() > 0){
@@ -67,8 +129,37 @@ void loop() {
       Serial.flush();
     }
     if(cmd != "") {
-      Serial.print("Comando recibido, procesaremos solicitud");
+      command(cmd);
     }
-    delay(500);
-    
+
+  
+}
+
+void command(String cmd){
+  switch(cmd.charAt(0)) {
+    case 'T': 
+      Serial.println(temp);
+      break;
+    case 'L':
+      Serial.println(luz_per);
+      break;
+    case 'H':
+      Serial.println(hum_per);
+      break;
+    case 'S':
+      switch(cmd.charAt(1)) {
+        case 'T':
+          maxCalor = cmd.substring(2).toInt();
+          break;
+        case 't':
+          minCalor = cmd.substring(2).toInt();
+          break;
+        case 'l':
+          luzMin = cmd.substring(2).toInt();
+          break;
+        case 'H':
+          humedadMax = cmd.substring(2).toInt();
+          break;
+      }
+  }
 }
